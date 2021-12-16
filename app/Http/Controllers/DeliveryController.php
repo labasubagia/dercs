ebd8356c1970d85c05c71af177418457140f4a8d
Laba Subagia <labasubagia22@gmail.com>
2021-12-16 16:38:06 +0800

    refactor delivery controller

--- app/Http/Controllers/DeliveryController.php
+++ app/Http/Controllers/DeliveryController.php
@@ -12,29 +12,29 @@ use Illuminate\Support\Facades\Auth;
 class DeliveryController extends Controller
 {
     //customer view tracking list
-    public function cViewTrackingList(){
-        $id = Auth::id();
-        $info = DB::select("select * from services inner join tracks 
-                            on services.id = tracks.id 
-                            where services.userID = '$id' AND tracks.trackProgress != 'Returned'");
-
+    public function cViewTrackingList()
+    {
+        $info = Service::where('services.userID', Auth::id())
+            ->where('tracks.trackProgress', '!=', 'Returned')
+            ->join('tracks', 'tracks.id', '=', 'services.id')
+            ->get();
         return view('customer.custList', compact('info'));
     }
 
     //customer view selected service tracking
-    public function cViewProgress(Request $req){
-        $serviceID = $req->id;
-
-        $info = DB::select("select tracks.updated_at, services.device, tracks.trackProgress, tracks.id
-                            from tracks 
-                            inner join services on tracks.id = services.id
-                            where tracks.id = '$serviceID'");
+    public function cViewProgress(Request $req)
+    {
+        $info = Track::where('tracks.id', $req->get('id'))
+            ->select("tracks.updated_at", "services.device", "tracks.trackProgress", "tracks.id")
+            ->join('services', 'services.id', '=', 'tracks.id')
+            ->get();
 
         return view('customer.custProgress', compact('info'));
     }
 
     //accept job page
-    public function viewStatus(){
+    public function viewStatus()
+    {
         $id = Auth::id();
         $unaccepted = DB::select("SELECT users.phoneNo, tracks.id, tracks.pickupAddress
                                 FROM users JOIN services ON users.id = services.userID
@@ -45,23 +45,25 @@ class DeliveryController extends Controller
                                 FROM users JOIN services ON users.id = services.userID
                                 JOIN tracks ON tracks.id = services.id
                                 WHERE services.status = 1 AND services.PIC = '$id'");
-        
+
         return view('rider.servicePage', compact('unaccepted', 'accepted'));
     }
 
     //to accept job
-    public function accept(Request $req){
+    public function accept(Request $req)
+    {
         $PIC = Auth::id();
 
         $trackID = $req->id;
-        $update = DB::select("update services set status = 1, PIC = '$PIC' 
+        $update = DB::select("update services set status = 1, PIC = '$PIC'
                             where id = '$trackID'");
 
-        return $this->viewStatus(); 
+        return $this->viewStatus();
     }
 
     //view service progress
-    public function viewAcceptedTask(Request $req){
+    public function viewAcceptedTask(Request $req)
+    {
         $id = Auth::id();
         $trackID = $req->id;
         $info = DB::select("select * from tracks where id = '$trackID'");
@@ -72,7 +74,8 @@ class DeliveryController extends Controller
     }
 
     //update service progress
-    public function updateProgress(Request $req){
+    public function updateProgress(Request $req)
+    {
         $time = $req->time;
         $date = $req->date;
         $trackID = $req->id;
@@ -81,45 +84,49 @@ class DeliveryController extends Controller
         $update = DB::select("update tracks set trackDate = '$date', trackTime = '$time',
                             trackProgress = '$trackProgress' where id = '$trackID'");
 
-        return $this->viewStatus(); 
+        return $this->viewStatus();
     }
 
-    
-    public function sViewTrackingList(){
+
+    public function sViewTrackingList()
+    {
         $info = DB::select("select * from services");
 
         return view('staff.staffList', compact('info'));
     }
 
-    public function sViewProgress(Request $req){
+    public function sViewProgress(Request $req)
+    {
         $serviceID = $req->id;
 
-        $info = DB::select("select * from tracks 
+        $info = DB::select("select * from tracks
                             inner join services on tracks.id = services.id
                             where tracks.id = '$serviceID'");
         return view('staff.staffProgress', compact('info'));
     }
 
     //staff receive item from rider
-    public function receive(Request $req){
-        $trackID = $req -> id;
+    public function receive(Request $req)
+    {
+        $trackID = $req->id;
         $result = "Confirm Received";
 
         $update = DB::select("update tracks set trackProgress = '$result'
                             where id = '$trackID'");
-        
-        $update1 = DB::select("update services set status = 2
-                            where id = '$trackID'");                    
 
-        $info = DB::select("select * from tracks 
+        $update1 = DB::select("update services set status = 2
+                            where id = '$trackID'");
+
+        $info = DB::select("select * from tracks
                            inner join services on tracks.id = services.id
                            where tracks.id = '$trackID'");
         return view('staff.staffProgress', compact('info'));
     }
 
     //customer received repaired item from rider
-    public function returnedDevice(Request $req){
-        $trackID = $req -> trackID;
+    public function returnedDevice(Request $req)
+    {
+        $trackID = $req->trackID;
 
         $update = DB::select("update tracks set trackProgress = 'Returned' where id = '$trackID'");
 
