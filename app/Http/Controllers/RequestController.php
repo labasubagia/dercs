9e596c6d15a052dd7e084606cf04d9da9a8c17bd
atrem13 <mertayoga17.my@gmail.com>
2022-01-03 11:25:46 +0800

    change raw sql to eloquent and fix redirect route after submit

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -14,34 +14,45 @@ class RequestController extends Controller
     //customer add service form
     public function displayCustInfo(){
         $id = Auth::id();
-        $info = DB::select("select * from users where id = '$id'");
+        // $info = DB::select("select * from users where id = '$id'");
+        $info = User::where('id',$id)->first();
 
         return view('customer.requestService', compact('info'));
     }
 
     //customer add service
     public function saveRequest(Request $req){
-        $userID = $req -> id;
-        $username = $req -> username;
-        $address = $req -> address;
+        // $userID = $req->id;
+        // $username = $req->username;
+        // $address = $req->address;
 
-        $add = new Service;
-        $add -> userID = $req -> id;
-        $add -> username = $req -> username;
-        $add -> device = $req -> device;
-        $add -> symptom = $req -> symptom;
-        $add -> save();
+        // $add = new Service;
+        // $add -> userID = $req -> id;
+        // $add -> username = $req -> username;
+        // $add -> device = $req -> device;
+        // $add -> symptom = $req -> symptom;
+        // $add -> save();
+        $service = Service::create([
+                                    'userID' => $req->id,
+                                    'username' => $req->username,
+                                    'device' => $req->device,
+                                    'symptom' => $req->symptom
+                                ]);
+        $track = Track::create([
+                                'userID' => $req->id,
+                                'pickupAddress' => $req->address
+                            ]);
 
-        $delivery = new Track;
-        $delivery -> userID = $userID;
-        $delivery -> pickupAddress = $address;
-        $delivery -> save();
+        // $delivery = new Track;
+        // $delivery -> userID = $userID;
+        // $delivery -> pickupAddress = $address;
+        // $delivery -> save();
 
         //$getID = DB::select("select id from tracks where pickupAddress = '$address' AND
         //                    userID = '$userID'");
 
         //$update = DB::select("update tracks set serviceID = $getID where id = $getID");
 
-        return redirect()->back() ->with('alert', 'Submit Successfully!');
+        return redirect('/cTrackList')->with('alert', 'Submit Successfully!');
     }
 }
