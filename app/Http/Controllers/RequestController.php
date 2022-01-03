2adc2d73435797f1bb3d673a1bfe794a6082b943
Muhammad Bin Syamsul Hafizee <95730917+MBSH99@users.noreply.github.com>
2022-01-03 11:53:47 +0800

    Revert "change raw sql to eloquent and fix redirect route after submit"

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -14,45 +14,34 @@ class RequestController extends Controller
     //customer add service form
     public function displayCustInfo(){
         $id = Auth::id();
-        // $info = DB::select("select * from users where id = '$id'");
-        $info = User::where('id',$id)->first();
+        $info = DB::select("select * from users where id = '$id'");
 
         return view('customer.requestService', compact('info'));
     }
 
     //customer add service
     public function saveRequest(Request $req){
-        // $userID = $req->id;
-        // $username = $req->username;
-        // $address = $req->address;
+        $userID = $req -> id;
+        $username = $req -> username;
+        $address = $req -> address;
 
-        // $add = new Service;
-        // $add -> userID = $req -> id;
-        // $add -> username = $req -> username;
-        // $add -> device = $req -> device;
-        // $add -> symptom = $req -> symptom;
-        // $add -> save();
-        $service = Service::create([
-                                    'userID' => $req->id,
-                                    'username' => $req->username,
-                                    'device' => $req->device,
-                                    'symptom' => $req->symptom
-                                ]);
-        $track = Track::create([
-                                'userID' => $req->id,
-                                'pickupAddress' => $req->address
-                            ]);
+        $add = new Service;
+        $add -> userID = $req -> id;
+        $add -> username = $req -> username;
+        $add -> device = $req -> device;
+        $add -> symptom = $req -> symptom;
+        $add -> save();
 
-        // $delivery = new Track;
-        // $delivery -> userID = $userID;
-        // $delivery -> pickupAddress = $address;
-        // $delivery -> save();
+        $delivery = new Track;
+        $delivery -> userID = $userID;
+        $delivery -> pickupAddress = $address;
+        $delivery -> save();
 
         //$getID = DB::select("select id from tracks where pickupAddress = '$address' AND
         //                    userID = '$userID'");
 
         //$update = DB::select("update tracks set serviceID = $getID where id = $getID");
 
-        return redirect('/cTrackList')->with('alert', 'Submit Successfully!');
+        return redirect()->back() ->with('alert', 'Submit Successfully!');
     }
 }
--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,5 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
-    protected $guarded = [];
 }
