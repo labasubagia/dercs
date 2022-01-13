9c54bab1890379d9bceb8be67c644a0d3d1005ce
NAinSanusi <95731516+NAinSanusi@users.noreply.github.com>
2022-01-13 23:09:15 +0800

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -5,6 +5,13 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +22,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('userType');
             $table->string('phoneNo');
@@ -23,7 +30,7 @@ class CreateUsersTable extends Migration
             $table->integer('status')->default(0);
             $table->string('licensePhoto')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->unique();
             $table->rememberToken();
             $table->timestamps();
         });
